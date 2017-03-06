7e7ee84d35e8ce53a3364ffcc7872a93a152cd47
DJ Versoza <djversoza@gmail.com>
2017-03-05 18:55:00 -0700

    added some styling to website

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$.backstretch([
+      "https://c1.staticflickr.com/9/8615/29874300531_14a25fd318_h.jpg"
+    , "https://c1.staticflickr.com/1/542/31662324561_8087d846d0_h.jpg"
+    , "https://c1.staticflickr.com/1/615/31531990833_7d6c6e2f69_h.jpg"
+  ], {duration: 3000, fade: 750});
+});
