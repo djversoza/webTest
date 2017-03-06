0f6eafd2a400e894c02f94e26a2d19f9b187c87c
DJ Versoza <djversoza@gmail.com>
2017-03-05 22:54:56 -0700

    added more photos to front page

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 	$.backstretch([
-      "https://c1.staticflickr.com/9/8615/29874300531_14a25fd318_h.jpg"
+		  "https://c2.staticflickr.com/2/1627/24177133685_d9a9608968_k.jpg"
+    , "https://c1.staticflickr.com/9/8615/29874300531_14a25fd318_h.jpg"
     , "https://c1.staticflickr.com/1/542/31662324561_8087d846d0_h.jpg"
     , "https://c1.staticflickr.com/1/615/31531990833_7d6c6e2f69_h.jpg"
+		, "https://c2.staticflickr.com/8/7079/27416608464_6a12418142_h.jpg"
+		, "https://c2.staticflickr.com/2/1705/26578406911_afb005569e_o.jpg"
+		, "https://c2.staticflickr.com/2/1512/25023324079_be04bf9751_k.jpg"
+		, "https://c2.staticflickr.com/2/1503/24963074759_e4bf21ad0d_k.jpg"
   ], {duration: 3000, fade: 750});
 });
